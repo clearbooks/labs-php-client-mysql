80e99487948bd47d3265948019815c1c1e41b675
Horváth Péter <peter@clearbooks.co.uk>
2015-08-21 15:16:08 +0200

    oh we don't need this file to be commited

--- config/db-config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-        "db.host"     => "127.0.0.1",
-        "db.port"     => 3306,
-        "db.name"     => "labs",
-        "db.user"     => "root",
-        "db.password" => "",
-];
