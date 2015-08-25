de240bf32ec7c37a0bf90f1c1708879eb444db9f
Horváth Péter <peter@clearbooks.co.uk>
2015-08-25 19:39:56 +0200

    refactoring

--- src/Db/Service/ToggleStorage.php
+++ src/Db/Service/ToggleStorage.php
@@ -78,24 +78,7 @@ class ToggleStorage implements ToggleRetriever, UserPolicyRetriever, GroupPolicy
      */
     public function setUserPolicy( $toggleId, $userId, $policy )
     {
-        if ( $policy === null ) {
-            $this->connection->delete( (string)$this->userPolicyTable,
-                                       [ "user_id" => $userId, "toggle_id" => $toggleId ] );
-            return;
-        }
-
-        $currentPolicy = $this->getUserPolicyOfToggle( $toggleId, $userId );
-
-        if ( $currentPolicy === null ) {
-            $this->connection->insert( (string)$this->userPolicyTable,
-                                       [ "user_id" => $userId, "toggle_id" => $toggleId,
-                                         "active"  => $policy ? 1 : 0 ] );
-            return;
-        }
-
-        $this->connection->update( (string)$this->userPolicyTable,
-                                   [ "active" => $policy ? 1 : 0 ],
-                                   [ "user_id" => $userId, "toggle_id" => $toggleId ] );
+        $this->setTogglePolicy( $toggleId, $userId, $policy, "user" );
     }
 
     /**
@@ -105,23 +88,38 @@ class ToggleStorage implements ToggleRetriever, UserPolicyRetriever, GroupPolicy
      */
     public function setGroupPolicy( $toggleId, $groupId, $policy )
     {
+        $this->setTogglePolicy( $toggleId, $groupId, $policy, "group" );
+    }
+
+    /**
+     * @param int $toggleId
+     * @param string $identityId
+     * @param bool|null $policy
+     * @param string $typeOfIdentity
+     * @throws \Doctrine\DBAL\Exception\InvalidArgumentException
+     */
+    public function setTogglePolicy( $toggleId, $identityId, $policy, $typeOfIdentity )
+    {
+        $table = $this->getPolicyTableByTypeOfIdentity( $typeOfIdentity );
+        $identityField = $this->getIdentityFieldByTypeOfIdentity( $typeOfIdentity );
+
         if ( $policy === null ) {
-            $this->connection->delete( (string)$this->groupPolicyTable,
-                                       [ "group_id" => $groupId, "toggle_id" => $toggleId ] );
+            $this->connection->delete( $table,
+                                       [ $identityField => $identityId, "toggle_id" => $toggleId ] );
             return;
         }
 
-        $currentPolicy = $this->getGroupPolicyOfToggle( $toggleId, $groupId );
+        $currentPolicy = $this->getPolicyOfToggle( $toggleId, $identityId, $typeOfIdentity );
 
         if ( $currentPolicy === null ) {
-            $this->connection->insert( (string)$this->groupPolicyTable,
-                                       [ "group_id" => $groupId, "toggle_id" => $toggleId,
+            $this->connection->insert( $table,
+                                       [ $identityField => $identityId, "toggle_id" => $toggleId,
                                          "active"  => $policy ? 1 : 0 ] );
         }
 
-        $this->connection->update( (string)$this->groupPolicyTable,
+        $this->connection->update( $table,
                                    [ "active" => $policy ? 1 : 0 ],
-                                   [ "group_id" => $groupId, "toggle_id" => $toggleId ] );
+                                   [ $identityField => $identityId, "toggle_id" => $toggleId ] );
     }
 
     /**
@@ -131,10 +129,34 @@ class ToggleStorage implements ToggleRetriever, UserPolicyRetriever, GroupPolicy
      */
     public function getUserPolicyOfToggle( $toggleId, $userId )
     {
+        return $this->getPolicyOfToggle( $toggleId, $userId, "user" );
+    }
+
+    /**
+     * @param int    $toggleId
+     * @param string $groupId
+     * @return bool|null
+     */
+    public function getGroupPolicyOfToggle( $toggleId, $groupId )
+    {
+        return $this->getPolicyOfToggle( $toggleId, $groupId, "group" );
+    }
+
+    /**
+     * @param int $toggleId
+     * @param string $identityId
+     * @param string $typeOfIdentity
+     * @return bool|null
+     */
+    public function getPolicyOfToggle( $toggleId, $identityId, $typeOfIdentity )
+    {
+        $table = $this->getPolicyTableByTypeOfIdentity( $typeOfIdentity );
+        $identityField = $this->getIdentityFieldByTypeOfIdentity( $typeOfIdentity );
+
         $queryBuilder = $this->connection->createQueryBuilder();
-        $queryBuilder->select( "active" )->from( (string)$this->userPolicyTable )
-                     ->where( "user_id = ? AND toggle_id = ?" );
-        $queryBuilder->setParameter( 0, $userId );
+        $queryBuilder->select( "active" )->from( $table )
+                     ->where( "{$identityField} = ? AND toggle_id = ?" );
+        $queryBuilder->setParameter( 0, $identityId );
         $queryBuilder->setParameter( 1, $toggleId );
 
         $active = $queryBuilder->execute()->fetchColumn();
@@ -146,23 +168,32 @@ class ToggleStorage implements ToggleRetriever, UserPolicyRetriever, GroupPolicy
     }
 
     /**
-     * @param int    $toggleId
-     * @param string $groupId
-     * @return bool|null
+     * @param string $typeOfIdentity
+     * @return string
      */
-    public function getGroupPolicyOfToggle( $toggleId, $groupId )
+    private function getPolicyTableByTypeOfIdentity( $typeOfIdentity )
     {
-        $queryBuilder = $this->connection->createQueryBuilder();
-        $queryBuilder->select( "active" )->from( (string)$this->groupPolicyTable )
-                     ->where( "group_id = ? AND toggle_id = ?" );
-        $queryBuilder->setParameter( 0, $groupId );
-        $queryBuilder->setParameter( 1, $toggleId );
+        switch ( $typeOfIdentity ) {
+            case "group":
+                return (string)$this->groupPolicyTable;
 
-        $active = $queryBuilder->execute()->fetchColumn();
-        if ( $active === false ) {
-            return null;
+            default:
+                return (string)$this->userPolicyTable;
         }
+    }
 
-        return $active === "0" ? false : true;
+    /**
+     * @param string $typeOfIdentity
+     * @return string
+     */
+    private function getIdentityFieldByTypeOfIdentity( $typeOfIdentity )
+    {
+        switch ( $typeOfIdentity ) {
+            case "group":
+                return "group_id";
+
+            default:
+                return "user_id";
+        }
     }
 }
